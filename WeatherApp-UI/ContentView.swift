dbe06f98380dd8c902e6544038ad9c1b8efb23ef
Lwis2804 <luis.enrique.enc@gmail.com>
2024-10-09 21:06:49 -0600

    [ADD] se agrega icono de nube y solo con formato asi como el texto del clima

--- WeatherApp-UI/ContentView.swift
+++ WeatherApp-UI/ContentView.swift
@@ -18,6 +18,16 @@ struct ContentView: View {
                 Text("CDMX, MEXICO").font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                VStack(spacing: 8){
+                    Image(systemName: "cloud.sun.fill") // imagen + modifiers
+                        .renderingMode(.original) //mantener el color original
+                        .resizable() //cambiar el tamanio original
+                        .aspectRatio(contentMode: .fit) //ajustar al frame
+                        .frame(width: 180, height: 180) //definir tamanio de frame
+                    Text("24°")
+                        .font(.system(size: 70, weight: .bold))
+                        .foregroundColor(.white)
+                }
                 Spacer()
             }
         }
